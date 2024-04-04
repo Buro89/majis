2093e3cc53aba54ae10bfdb743a7ed4d9f8004df
Karola <info@karlijnroex.net>
2024-04-04 22:16:35 +0200

    Improved game (user has more choice and code is more defensive)

--- dobbelsteen.py
+++ dobbelsteen.py
@@ -18,88 +18,120 @@ def throw_dice_for_all(participants):
         participant.scores.append(participant.dice)
         print(f"{participant.name} has thrown a {participant.dice}")
 
-resident1 = Residents()
-resident1.name = "Altan"
-resident1.id = 1
-resident1.team = "Majis"
-resident2 = Residents()
-resident2.name = "Goromi"
-resident2.id = 2
-resident2.team = "Majis"
-resident3 = Residents()
-resident3.name = "Kiryu"
-resident3.id = 3
-resident3.team = "Dragon Team"
-resident4 = Residents()
-resident4.name = "Nozomi"
-resident4.id = 4
-resident4.team = "Majis"
-resident5 = Residents()
-resident5.name = "Inizio"
-resident5.id = 5
-resident5.team = "Majis"
-resident6 = Residents()
-resident6.name = "Cyber"
-resident6.id = 6
-resident6.team = "Majis"
-resident7 = Residents()
-resident7.name = "Yamai"
-resident7.id = 7
-resident7.team = "Baddies"
-resident8 = Residents()
-resident8.name = "Sasaki"
-resident8.id = 8
-resident8.team = "Baddies"
-resident9 = Residents()
-resident9.name = "Okita"
-resident9.id = 9
-resident9.team = "Japanese Plushies"
-resident10 = Residents()
-resident10.name = "Ryoma"
-resident10.id = 10
-resident10.team = "Japanese Plushies"
-resident11 = Residents()
-resident11.name = "Saejima"
-resident11.id = 11
-resident11.team = "Hydrogenpink Plushies"
-resident12 = Residents()
-resident12.name = "Nagakura"
-resident12.id = 12
-resident12.team = "Japanese Plushies"
-resident13 = Residents()
-resident13.name = "Futaba"
-resident13.id = 13
-resident13.team = "Phantom Thieves"
-resident14 = Residents()
-resident14.name = "Jokah"
-resident14.id = 14
-resident14.team = "Phantom Thieves"
-resident15 = Residents()
-resident15.name = "Star"
-resident15.id = 15
-resident15.team = "Japanese Plushies"
-resident16 = Residents()
-resident16.name = "Majima"
-resident16.id = 16
-resident16.team = "Hydrogenpink Plushies"
-resident17 = Residents()
-resident17.name = "Karola"
-resident17.id = 17
-resident17.team = "House Lady"
-resident18 = Residents()
-resident18.name = "Nishitani"
-resident18.id = 18
-resident18.team = "Baddies"
-resident19 = Residents()
-resident19.name = "Bunchan"
-resident19.id = 19
-resident19.team = "Japanese Plushies"
-resident20 = Residents()
-resident20.name = "Kirby"
-resident20.id = 20
-resident20.team = "Japanese Plushies"
 
-participants = [resident1, resident2, resident3, resident4, resident5, resident6, resident7, resident8, resident9, resident10, resident11, resident12, resident13, resident14, resident15, resident16, resident17, resident18, resident19, resident20]
+resident1 = Residents()
+resident1.id = 1
+resident2 = Residents()
+resident2.id = 2
+resident3 = Residents()
+resident3.id = 3
+resident4 = Residents()
+resident4.id = 4
+resident5 = Residents()
+resident5.id = 5
+resident6 = Residents()
+resident6.id = 6
+resident7 = Residents()
+resident7.id = 7
+resident8 = Residents()
+resident8.id = 8
+resident9 = Residents()
+resident9.id = 9
+resident10 = Residents()
+resident10.id = 10
+resident11 = Residents()
+resident11.id = 11
+resident12 = Residents()
+resident12.id = 12
+resident13 = Residents()
+resident13.id = 13
+resident14 = Residents()
+resident14.id = 14
+resident15 = Residents()
+resident15.id = 15
+resident16 = Residents()
+resident16.id = 16
+resident17 = Residents()
+resident17.id = 17
+resident18 = Residents()
+resident18.id = 18
+resident19 = Residents()
+resident19.id = 19
+resident20 = Residents()
+resident20.id = 20
+
+preference = ""
+while preference.upper() != "A" and preference.upper() != "B":
+    preference = input("""Let the residents of my house participate, or choose other participants?
+                    A. Residents
+                    B. Define my own """)
+if preference.upper() not in ["A", "B"]:
+    print("\nYou didn't answer A or B. Try again.\n")
+elif preference.upper() == "B":
+    number_of_participants_entered = 0
+    command = ""
+    while command.upper() != "B":
+        for i in range(1, 21):
+            name = input(f"Participant{i}'s name: ")
+            if name:
+                number_of_participants_entered += 1
+                globals()[f"resident{i}"].name = name
+                globals()[f"resident{i}"].team = input(f"Choose a Team for Participant{i}: ")
+                if number_of_participants_entered >= 2:
+                    command = input("""Do you want to enter more new participants?
+                                    A. continue entering
+                                    B. no, let's get to the actual game """)
+                    if command.upper() == "B":
+                        break
+elif preference.upper() == "A":
+    number_of_participants_entered = 20
+    resident1.name = "Altan"
+    resident1.team = "Majis"
+    resident2.name = "Goromi"
+    resident2.team = "Majis"
+    resident3.name = "Kiryu"
+    resident3.team = "Dragon Team"
+    resident4.name = "Nozomi"
+    resident4.team = "Majis"
+    resident5.name = "Inizio"
+    resident5.team = "Majis"
+    resident6.name = "Cyber"
+    resident6.team = "Majis"
+    resident7.name = "Yamai"
+    resident7.team = "Baddies"
+    resident8.name = "Sasaki"
+    resident8.team = "Baddies"
+    resident9.name = "Okita"
+    resident9.team = "Japanese Plushies"
+    resident10.name = "Ryoma"
+    resident10.team = "Japanese Plushies"
+    resident11.name = "Saejima"
+    resident11.team = "Hydrogenpink Plushies"
+    resident12.name = "Nagakura"
+    resident12.team = "Japanese Plushies"
+    resident13.name = "Futaba"
+    resident13.team = "Phantom Thieves"
+    resident14.name = "Jokah"
+    resident14.team = "Phantom Thieves"
+    resident15.name = "Star"
+    resident15.team = "Japanese Plushies"
+    resident16.name = "Majima"
+    resident16.team = "Hydrogenpink Plushies"
+    resident17.name = "Karola"
+    resident17.team = "House Lady"
+    resident18.name = "Nishitani"
+    resident18.team = "Baddies"
+    resident19.name = "Bunchan"
+    resident19.team = "Japanese Plushies"
+    resident20.name = "Kirby"
+    resident20.team = "Japanese Plushies"
+    pass
+else:
+    print("\nError: Something went wrong. Try again.\n")
+
+participants = []
+for i in range(1, number_of_participants_entered + 1):
+    participants.append(globals()[f"resident{i}"])
 
 run_another_round = True
 while run_another_round:
@@ -116,9 +148,9 @@ while run_another_round:
             max_resident.append(resident)
 
     if len(max_resident) == 1:
-        print(f"\n\nThe maji with the highest score is {max_resident[0].name} with a score of {max_resident[0].dice}.")
+        print(f"\n\nThe participant with the highest score is {max_resident[0].name} with a score of {max_resident[0].dice}.")
     else:
-        print(f"\n\nThe majis with the highest score are:")
+        print(f"\n\nThe participants with the highest score are:")
         for participant in max_resident:
             print(f"{participant.name} with a score of {participant.dice}.")
 
@@ -141,5 +173,8 @@ df = pd.DataFrame(data)
 
 df.to_excel(os.path.join(working_dir, "dice_scores.xlsx"), index=False)
 
-# More about creating dataframes from list https://www.geeksforgeeks.org/create-a-pandas-dataframe-from-lists/
-Residents()
+print("\n\nDataframe created... Now generating the scores' results...\n\n")
+import scores_analysis_and_visualisation
+scores_analysis_and_visualisation
+
+
--- scores_analysis_and_visualisation.py
+++ scores_analysis_and_visualisation.py
@@ -38,143 +38,162 @@ def save_and_show_plot(filename, transparency):
     plt.savefig(f"{the_chosen_path}{filename}.png", dpi=500, transparent=transparency, bbox_inches="tight")
     plt.show()
 
+number_of_participants = data.shape[0]
+number_of_rounds = data.shape[1] - 3
 
-number_of_rounds = int(input("""
-                             Hello. Now we're going to analyse the score data and output some graphs!
+print(f"""Check: 
+- you have {number_of_participants} partcipants;
+- you have {number_of_rounds} rounds
+""")
 
-                             But first of all, we need some info from you: How many rounds did you play?
-                             Don't remember because you were just passionately smashing 'yes' all the time? Then, 
-                             open "dice_scores.xlsx and see how many Round_x columns there are (where x is a number).
-                             
-                             Please insert the number of rounds: """))
-round_columns = [f"Round_{round_number}" for round_number in range(1, number_of_rounds + 1)]
-data["total_score"] = data[round_columns].sum(axis=1)
-print("Total score per participant:")
-for i in range(1, 21):
-    for index, row in data.iterrows():
-        if row["ID"] == i:
-            total_score = row["total_score"]
-            print(f"{row['Participant']} has a total score of {total_score}")
+counter = 0
+while counter < 1:
+    introtext = input("""
+                                Hello. Now we're going to analyse the score data and output some graphs!
 
-print(get_sumstats("total_score", data))
+                                Press a key to continue! OR type 'stop' to quit! """)
 
+    if introtext.lower() == "stop":
+        break
+    
+    counter += 1
 
+    round_columns = [f"Round_{round_number}" for round_number in range(1, number_of_rounds + 1)]
+    data["total_score"] = data[round_columns].sum(axis=1)
+    print("Total score per participant:")
+    for i in range(1, number_of_participants + 1):
+        for index, row in data.iterrows():
+            if row["ID"] == i:
+                total_score = row["total_score"]
+                print(f"{row['Participant']} has a total score of {total_score}")
 
-
-average_total_score = data["total_score"].mean()
-data["final_evaluation"] = data["total_score"].apply(lambda x: 1 if x >= average_total_score else 0)
-
-for i in range(1, 21):
-    for index, row in data.iterrows():
-        if row["ID"] == i:
-            total_score = row["total_score"]
-            print(f"{row['Participant']} has a total score of {total_score}")
-
-
-for i in range(1, number_of_rounds + 1):
-    globals()[f"highest_round{i}"] = data[f"Round_{i}"].max()
-    data[f"top_participant{i}"] = (data[f"Round_{i}"] == globals()[f"highest_round{i}"])
-
-rank_mapping = {
-    False: 0,
-    True: 1}
-for i in range(1, number_of_rounds + 1):
-    data[f"top_participant{i}_dummy"] = data[f"top_participant{i}"].map(rank_mapping)
-    data[f"top_participant{i}_dummy"] = data[f"top_participant{i}_dummy"].astype(int)
-
-top_position_columns = [f"top_participant{round_number}_dummy" for round_number in range(1, number_of_rounds + 1)]
-data["total_top_positions"] = data[top_position_columns].sum(axis=1)
-print("Total times a participant was at top position in all rounds:")
-for i in range(1, 21):
-    for index, row in data.iterrows():
-        if row["ID"] == i:
-            total_top_positions = row["total_top_positions"]
-            print(f"{row['Participant']} was in the top at {total_top_positions}")
-
-print(get_sumstats("total_top_positions", data))
-
-
-# General overview of the dataframe
-print("\n\n" + (10*"-") + "Global info about the dataframe" + (10*"-") + "\n\n")
-data.info()
-print(f"\n\n {data.head} \n\n")
-
-
-sns.set(style="whitegrid", font="Garamond")
-plt.rcParams["font.family"] = "Garamond"
-plt.figure(figsize=(14, 9))
-
-above_average_score_data = data[data["final_evaluation"] == 1]
-below_average_score_data = data[data["final_evaluation"] == 0]
-
-sns.countplot(data=data, x="Team", hue="final_evaluation", dodge=True, palette={0: "#d18e75", 1: "#176482"}, alpha=0.7)
-plt.ylabel("Quantity", fontsize=32, labelpad=18)
-plt.xlabel("Teams", fontsize=32, labelpad=18)
-plt.xticks(fontsize=24, rotation=90)
-plt.yticks(fontsize=24)
-plt.ylim(0, 5)
-plt.legend(title="Evaluation", loc="upper right", fontsize=22, title_fontsize="24", labels=["Below average", "Above average"])
-#save_and_show_plot("scores", False)
-
-print_proportion_table_per_group("final_evaluation", "Team", data)
-
-
-sns.set(style="whitegrid", font="Garamond")
-plt.rcParams["font.family"] = "Garamond"
-plt.figure(figsize=(14, 6))
-
-sns.barplot(data=data, x="Participant", y="total_score", palette={"Cyber": "#d18e75", "Futaba": "#176482", "Okita": "pink", "Nagakura": "#F2C6B6", "Jokah": "#89c6b6", "Yamai": "#60272b", "Karola": "#af793d", "Goromi": "orange", "Kirby": "#5ea46b", "Nozomi": "red", "Bunchan": "#60cea4", "Inizio": "brown", "Star": "black", "Nishitani" :"salmon", "Saejima": "darkgreen", "Altan": "lightblue", "Ryoma": "lightgreen", "Sasaki": "#321B12", "Majima": "#34504e", "Kiryu": "#0f3c48"}, alpha = 0.7, width=1)
-plt.xlabel("Participant", fontsize=32, labelpad=18)
-plt.ylabel("Total Score", fontsize=32, labelpad=18)
-plt.xticks(fontsize=24, rotation=90)
-plt.yticks(fontsize=24)
-plt.ylim(0, 110)
-#save_and_show_plot("scores2", False)
-
-
-print_proportion_table_per_group("total_score", "Participant", data)
+    print(get_sumstats("total_score", data))
 
 
 
-sns.set(style="whitegrid", font="Garamond")
-plt.rcParams["font.family"] = "Garamond"
-plt.figure(figsize=(14, 6))
 
-sns.barplot(data=data, x="Participant", y="total_top_positions", palette={"Cyber": "#d18e75", "Futaba": "#176482", "Okita": "pink", "Nagakura": "#F2C6B6", "Jokah": "#89c6b6", "Yamai": "#60272b", "Karola": "#af793d", "Goromi": "orange", "Kirby": "#5ea46b", "Nozomi": "red", "Bunchan": "#60cea4", "Inizio": "brown", "Star": "black", "Nishitani" :"salmon", "Saejima": "darkgreen", "Altan": "lightblue", "Ryoma": "lightgreen", "Sasaki": "#321B12", "Majima": "#34504e", "Kiryu": "#0f3c48"}, alpha = 0.7, width=1)
-plt.xlabel("Participant", fontsize=32, labelpad=18)
-plt.ylabel("Total rounds at top", fontsize=32, labelpad=18)
-plt.xticks(fontsize=24, rotation=90)
-plt.yticks(fontsize=24)
-plt.ylim(0, 6)
-#save_and_show_plot("scores3", False)
+    average_total_score = data["total_score"].mean()
+    data["final_evaluation"] = data["total_score"].apply(lambda x: 1 if x >= average_total_score else 0)
+
+    for i in range(1, number_of_participants + 1):
+        for index, row in data.iterrows():
+            if row["ID"] == i:
+                total_score = row["total_score"]
+                print(f"{row['Participant']} has a total score of {total_score}")
 
 
-print_proportion_table_per_group("total_top_positions", "Participant", data)
+    for i in range(1, number_of_rounds + 1):
+        globals()[f"highest_round{i}"] = data[f"Round_{i}"].max()
+        data[f"top_participant{i}"] = (data[f"Round_{i}"] == globals()[f"highest_round{i}"])
+
+    rank_mapping = {
+        False: 0,
+        True: 1}
+    for i in range(1, number_of_rounds + 1):
+        data[f"top_participant{i}_dummy"] = data[f"top_participant{i}"].map(rank_mapping)
+        data[f"top_participant{i}_dummy"] = data[f"top_participant{i}_dummy"].astype(int)
+
+    top_position_columns = [f"top_participant{round_number}_dummy" for round_number in range(1, number_of_rounds + 1)]
+    data["total_top_positions"] = data[top_position_columns].sum(axis=1)
+    print("Total times a participant was at top position in all rounds:")
+    for i in range(1, number_of_participants + 1):
+        for index, row in data.iterrows():
+            if row["ID"] == i:
+                total_top_positions = row["total_top_positions"]
+                print(f"{row['Participant']} was in the top at {total_top_positions}")
+
+    print(get_sumstats("total_top_positions", data))
 
 
-sns.set(style="whitegrid", font="Garamond")
-plt.rcParams["font.family"] = "Garamond"
-plt.figure(figsize=(14, 9))
-
-sns.barplot(data=data, x="Team", y="total_top_positions", hue="Team", ci=None, palette={"Majis": "#d18e75", "Dragon Team": "#176482", "Baddies": "#89c6b6", "Japanese Plushies": "pink", "Hydrogenpink Plushies": "#F2C6B6", "Phantom Thieves": "orange", "House Lady": "#60272b"}, alpha=0.7)
-plt.ylabel("Total rounds at top", fontsize=32, labelpad=18)
-plt.xlabel("Teams", fontsize=32, labelpad=18)
-plt.xticks(fontsize=24, rotation=90)
-plt.yticks(fontsize=24)
-plt.ylim(0, 5)
-plt.legend(title="Team", loc="upper right", fontsize=22, title_fontsize="24", labels=["Majis", "Dragon Team", "Baddies", "Japanese Plushies", "Hydrogenpink Plushies", "Phantom Thieves", "House Lady"], ncol=3)
-save_and_show_plot("scores4", False)
-
-print_proportion_table_per_group("total_top_positions", "Team", data)
+    # General overview of the dataframe
+    print("\n\n" + (10*"-") + "Global info about the dataframe" + (10*"-") + "\n\n")
+    data.info()
+    print(f"\n\n {data.head} \n\n")
 
 
-data.to_excel(os.path.join(the_chosen_path, "dice_scores_processed.xlsx"), index=False)
+    sns.set(style="whitegrid", font="Garamond")
+    plt.rcParams["font.family"] = "Garamond"
+    plt.figure(figsize=(14, 9))
 
-input("Press key to end ")
+    above_average_score_data = data[data["final_evaluation"] == 1]
+    below_average_score_data = data[data["final_evaluation"] == 0]
 
-# Actiepunten:
-# Maak de grafieken nog mooier! Legenda op perfecte plek, andere twee grafieken ook een legenda
-# 'Trendgrafiek van de Majis van hoe goed zij scoren door de loop van alle rondes heen
-# Alle grafieken en tevens tabellen naar een Word document pipen, met nog wat tekst erbij! Allemaal geautomatiseerd!
-# Deze code wat netter maken
-# dobbelsteen.py naar scores_analyses_and_visualisation.py laten overgaan
+    sns.countplot(data=data, x="Team", hue="final_evaluation", dodge=True, palette={0: "#d18e75", 1: "#176482"}, alpha=0.7)
+    plt.ylabel("Quantity", fontsize=32, labelpad=18)
+    plt.xlabel("Teams", fontsize=32, labelpad=18)
+    plt.xticks(fontsize=24, rotation=90)
+    plt.yticks(fontsize=24)
+    plt.ylim(0, 5)
+    plt.legend(title="Evaluation", loc="upper right", fontsize=22, title_fontsize="24", labels=["Below average", "Above average"])
+    #save_and_show_plot("scores", False)
+
+    print_proportion_table_per_group("final_evaluation", "Team", data)
+
+
+    sns.set(style="whitegrid", font="Garamond")
+    plt.rcParams["font.family"] = "Garamond"
+    plt.figure(figsize=(14, 6))
+
+    participant_names = data["Participant"].tolist()
+
+    participant_colors = {
+        participant: sns.color_palette("rocket", n_colors=len(participant_names))[i]
+        for i, participant in enumerate(participant_names)
+    }
+
+    sns.barplot(data=data, x="Participant", y="total_score", palette=participant_colors, alpha = 0.7, width=1, errorbar = None)
+    plt.xlabel("Participant", fontsize=32, labelpad=18)
+    plt.ylabel("Total Score", fontsize=32, labelpad=18)
+    plt.xticks(fontsize=24, rotation=90)
+    plt.yticks(fontsize=24)
+    plt.ylim(0, 110)
+    save_and_show_plot("scores2", False)
+
+
+    print_proportion_table_per_group("total_score", "Participant", data)
+
+
+
+    #sns.set(style="whitegrid", font="Garamond")
+    #plt.rcParams["font.family"] = "Garamond"
+    #plt.figure(figsize=(14, 6))
+
+    #sns.barplot(data=data, x="Participant", y="total_top_positions", palette={"Cyber": "#d18e75", "Futaba": "#176482", "Okita": "pink", "Nagakura": "#F2C6B6", "Jokah": "#89c6b6", "Yamai": "#60272b", "Karola": "#af793d", "Goromi": "orange", "Kirby": "#5ea46b", "Nozomi": "red", "Bunchan": "#60cea4", "Inizio": "brown", "Star": "black", "Nishitani" :"salmon", "Saejima": "darkgreen", "Altan": "lightblue", "Ryoma": "lightgreen", "Sasaki": "#321B12", "Majima": "#34504e", "Kiryu": "#0f3c48"}, alpha = 0.7, width=1)
+    #plt.xlabel("Participant", fontsize=32, labelpad=18)
+    #plt.ylabel("Total rounds at top", fontsize=32, labelpad=18)
+    #plt.xticks(fontsize=24, rotation=90)
+    #plt.yticks(fontsize=24)
+    #plt.ylim(0, 6)
+    #save_and_show_plot("scores3", False)
+
+
+    #print_proportion_table_per_group("total_top_positions", "Participant", data)
+
+
+    #sns.set(style="whitegrid", font="Garamond")
+    #plt.rcParams["font.family"] = "Garamond"
+    #plt.figure(figsize=(14, 9))
+
+    #sns.barplot(data=data, x="Team", y="total_top_positions", hue="Team", ci=None, palette={"Majis": "#d18e75", "Dragon Team": "#176482", "Baddies": "#89c6b6", "Japanese Plushies": "pink", "Hydrogenpink Plushies": "#F2C6B6", "Phantom Thieves": "orange", "House Lady": "#60272b"}, alpha=0.7)
+    #plt.ylabel("Total rounds at top", fontsize=32, labelpad=18)
+    #plt.xlabel("Teams", fontsize=32, labelpad=18)
+    #plt.xticks(fontsize=24, rotation=90)
+    #plt.yticks(fontsize=24)
+    #plt.ylim(0, 5)
+    #plt.legend(title="Team", loc="upper right", fontsize=22, title_fontsize="24", labels=["Majis", "Dragon Team", "Baddies", "Japanese Plushies", "Hydrogenpink Plushies", "Phantom Thieves", "House Lady"], ncol=3)
+    #save_and_show_plot("scores4", False)
+
+    #print_proportion_table_per_group("total_top_positions", "Team", data)
+
+
+    #data.to_excel(os.path.join(the_chosen_path, "dice_scores_processed.xlsx"), index=False)
+
+
+    input("Press key to end ")
+
+    # Actiepunten:
+    # Maak de grafieken nog mooier! Legenda op perfecte plek, andere twee grafieken ook een legenda
+    # 'Trendgrafiek van de Majis van hoe goed zij scoren door de loop van alle rondes heen
+    # Alle grafieken en tevens tabellen naar een Word document pipen, met nog wat tekst erbij! Allemaal geautomatiseerd!
+    # Deze code wat netter maken
+    # dobbelsteen.py naar scores_analyses_and_visualisation.py laten overgaan
